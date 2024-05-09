e6306f993ec308faf589a8512ebb010f4b528c9b
BreTre <bctrepal@gmail.com>
2024-05-08 21:35:23 -0400

    initial commit, set up schema & started seeding

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO departments (dept_name)
+VALUES ('Accounting'),
+       ('Finance'),
+       ('Human Resources'),
+       ('IT'),
+       ('Marketing'),
+       ('R & D'),
+       ('Management');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('CEO'),
+       ('CFO'),
+       ('CMO'),
+       ('CTO'),
+       ('President'),
+       ('Vice President'),
+       ('Manager'),
+       ();
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('Ursula', 'Leroi', ),
+       (),
+       (),
+       (),
+       (),
+       (),
+       ();
+       
